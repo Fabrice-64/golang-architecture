4e41a91c382ae48f80821846b9812a21b9e55fed
Fabrice-64 <fab.jaouen@gmail.com>
2021-11-11 20:48:03 +0100

    Add auth files

--- 04_client_server/03_jwt/authapp/auth/auth.go
+++ 04_client_server/03_jwt/authapp/auth/auth.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"errors"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -28,6 +29,29 @@ func (j *JwtWrapper) GenerateToken(email string) (signedToken string, err error)
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	signedToken, err = token.SignedString([]byte(j.SecretKey))
 	if err != nil {
+		return
+	}
+	return
+}
+
+func (j *JwtWrapper) ValidateToken(signedToken string) (claims *JwtClaim, err error) {
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&JwtClaim{},
+		func(t *jwt.Token) (interface{}, error) {
+			return []byte(j.SecretKey), nil
+		},
+	)
+	if err != nil {
+		return
+	}
+	claims, ok := token.Claims.(*JwtClaim)
+	if !ok {
+		err = errors.New("could not parse claims")
+		return
+	}
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		err = errors.New("JWT is expired")
 		return
 	}
 	return
