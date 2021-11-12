3b70a28b2119d0b146a0f54bea46a33fc5ff34f2
Fabrice-64 <fab.jaouen@gmail.com>
2021-11-12 15:17:13 +0100

    Add Login

--- 04_client_server/03_jwt/authapp/controllers/public.go
+++ 04_client_server/03_jwt/authapp/controllers/public.go
@@ -2,9 +2,13 @@ package controllers
 
 import (
 	"log"
+	"net/http"
 
+	"github.com/Fabrice-64/golang-architecture/04_client_server/03_jwt/authapp/auth"
+	"github.com/Fabrice-64/golang-architecture/04_client_server/03_jwt/authapp/database"
 	"github.com/Fabrice-64/golang-architecture/04_client_server/03_jwt/authapp/models"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func Signup(c *gin.Context) {
@@ -37,4 +41,54 @@ func Signup(c *gin.Context) {
 		return
 	}
 	c.JSON(200, user)
+}
+
+type LoginPayload struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
+
+func Login(c *gin.Context) {
+	var payload LoginPayload
+	var user models.User
+
+	err := c.ShouldBindJSON(&payload)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "invalid json",
+		})
+		c.Abort()
+		return
+	}
+	result := database.GlobalDB.Where("email = ?", payload.Email).First(&user)
+	if result.Error == gorm.ErrRecordNotFound {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"msg": "Invalid user Credentials",
+		})
+		c.Abort()
+		return
+	}
+	jwtWrapper := auth.JwtWrapper{
+		SecretKey:       "verysecretkey",
+		Issuer:          "AuthService",
+		ExpirationHours: 24,
+	}
+	signedToken, err := jwtWrapper.GenerateToken(user.Email)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": "error signing token",
+		})
+		c.Abort()
+		return
+	}
+	tokenResponse := LoginResponse{
+		Token: signedToken,
+	}
+	c.JSON(http.StatusOK, tokenResponse)
+	return
 }
--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## Generate Curl Commands:
 https://curlbuilder.com/
 
+## JWT chapter origin
+The code source of JWT Chapter is to be found at: 
+https://betterprogramming.pub/hands-on-with-jwt-in-golang-8c986d1bb4c0
+Author: Hamza Boughraira
+
+
