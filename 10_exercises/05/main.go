b0cce417664da92e46606d58c0c170212af42c0b
Fabrice-64 <fab.jaouen@gmail.com>
2021-12-11 08:56:55 +0100

    create skeleton for exercise 5

---
+++ 10_exercises/05/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type User struct {
+	First    string
+	Email    string
+	Password []byte
+}
+
+var secretKey = "MySecretKey"
+var dbUsers = map[string]User{}
+var dbSession = map[string]string{}
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func register(w http.ResponseWriter, req *http.Request) {
+	tpl.ExecuteTemplate(w, "register.gohtml", nil)
+}
+
+func login(w http.ResponseWriter, req *http.Request) {
+	tpl.ExecuteTemplate(w, "login.gohtml", nil)
+}
